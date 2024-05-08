33bf4290a93926e422415fd0c3a0eb9ba8caf453
alison <alisonherrera442@gmail.com>
2024-05-07 21:21:10 -0500

    add ejercicios

---
+++ Ejercicios_length/ejercicio1.js
@@ -0,0 +1,14 @@
+let estudiantes = ["alison", "juan", "carlos"];
+
+if (estudiantes.length == 0) {
+    console.log ("el arreglo de estudiantes esta vacio");
+}else{
+    console.log ("la cantidad de estudiantes que hay es:", estudiantes.length);
+   
+
+    }
+
+
+
+
+
