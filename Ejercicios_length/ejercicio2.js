c0c3889ca7c19f83b429ea70a5715ea89e004fa1
Alison Gonzalez Herrera <alisonherrera442@gmail.com>
2024-05-14 16:20:37 -0500

    ejercicios

---
+++ Ejercicios_length/ejercicio2.js
@@ -0,0 +1,7 @@
+let suma = 0;
+const numeros = [5, 10, 15, 20];
+
+for (let i = 0; i < numeros.length; i++){
+    suma += numeros[i];
+    console.log (suma);
+}
---
+++ Ejercicios_length/ejercicio3.js
@@ -0,0 +1,10 @@
+let productos = [
+    { nombre: "Blusa", precio: 30, stock: 25 },
+    { nombre: "Jean", precio: 50, stock: 20 },
+    { nombre: "Zapatos", precio: 80, stock: 15 }
+];
+
+console.log("Nombre del primer producto: " + productos[0].nombre);
+console.log("Precio del primer producto: $" + productos[0].precio);
+
+console.log("Cantidad de productos: " + productos.length);
